879669b0faa51310ae71d4f85df852e8d3abc6c5
jcontrerasf <jcontrerasf@users.noreply.github.com>
2019-05-01 15:56:37 -0400

    creo modulo alu

---
+++ Exp5/Exp5.srcs/sources_1/new/alu.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.05.2019 16:55:02
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+
+    );
+endmodule
