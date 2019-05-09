31706441ff35ee6347c32f02b8e65050f0eae8b3
jcontrerasf <jcontrerasf@users.noreply.github.com>
2019-05-08 21:32:06 -0400

    funcionando

--- btn-count/btn-count.srcs/sources_1/new/TDM.sv
+++ btn-count/btn-count.srcs/sources_1/new/TDM.sv
@@ -18,6 +18,22 @@
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
+
+//Plantilla de instanciacion:
+//TDM multiplex(.clk(reloj),
+//    .reset(CPU_RESET),
+//    .d1(salida[3:0]),
+//    .d2(salida[7:4]),
+//    .d3(nada),
+//    .d4(nada),
+//    .d5(nada),
+//    .d6(nada),
+//    .d7(nada),
+//    .d8(nada),
+//    .anodos(AN)
+//,   .bcd(bcdout)
+//,   .usados(uso));
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -25,6 +41,7 @@ module TDM
 #(parameter digitos = 8)
     (input logic clk,reset,
     input logic [3:0] d1,d2,d3,d4,d5,d6,d7,d8,
+    input logic [7:0] usados, //los display usados deben tener 1 // estan en el mismo orden
     output logic [7:0] anodos,
     output logic [3:0] bcd
     );
@@ -39,35 +56,59 @@ module TDM
     
     case (contador)
         3'b000: begin
-            anodos <= ~8'b00000001;
+            if (usados[0])
+                anodos <= ~8'b00000001;
+            else
+                anodos <= ~8'b00000000;
             bcd <= d1;
         end
         3'b001: begin
-            anodos <= ~8'b00000010;
+            if (usados[1])
+                anodos <= ~8'b00000010;
+            else
+                anodos <= ~8'b00000000;
             bcd <= d2;
         end
-        3'b010: begin 
-            anodos <= ~8'b00000100;
+        3'b010: begin
+            if (usados[2])
+                anodos <= ~8'b00000100;
+            else
+                anodos <= ~8'b00000000;
             bcd <= d3;
         end
         3'b011: begin
-            anodos <= ~8'b00001000;
+            if (usados[3])
+                anodos <= ~8'b00001000;
+            else
+                anodos <= ~8'b00000000;
             bcd <= d4;
         end
         3'b100: begin
-            anodos <= ~8'b00010000;
+            if (usados[4])
+                anodos <= ~8'b00010000;
+            else
+                anodos <= ~8'b00000000;
             bcd <= d5;
         end
         3'b101: begin
-            anodos <= ~8'b00100000;
+            if (usados[5])
+                anodos <= ~8'b00100000;
+            else
+                anodos <= ~8'b00000000;
             bcd <= d6;
         end
         3'b110: begin
-            anodos <= ~8'b01000000;
+            if (usados[6])
+                anodos <= ~8'b01000000;
+            else
+                anodos <= ~8'b00000000;
             bcd <= d7;
         end
         3'b111: begin
-            anodos <= ~8'b10000000;
+            if (usados[7])
+                anodos <= ~8'b10000000;
+            else
+                anodos <= ~8'b00000000;
             bcd <= d8;
         end
         default: begin
--- btn-count/btn-count.srcs/sources_1/new/main.sv
+++ btn-count/btn-count.srcs/sources_1/new/main.sv
@@ -28,11 +28,13 @@ module main(
     );
     
     logic reloj;
-    logic [6:0] nada;
+    logic [3:0] nada;
     logic [7:0] salida;
     logic [3:0] bcdout;
-    logic carry1;
-    assign nada = 7'b0000000;
+    logic [7:0] uso;
+    
+    assign nada = 4'b0000;
+    assign uso = 8'b00000011;
     
     divider #(.maxcount(104166)) uno(.clkin(CLK100MHZ),.reset(CPU_RESET),.clkout(reloj));
     contador #(.bits(8)) unidad(.clk(CLK100MHZ),.btn(BTNU),.reset(BTNC),.count(salida));
@@ -42,7 +44,7 @@ module main(
     .d2(salida[7:4]),
     .d3(nada),
     .d4(nada),
-    .d5(nada),.d6(nada),.d7(nada),.d8(nada),.anodos(AN),.bcd(bcdout));
+    .d5(nada),.d6(nada),.d7(nada),.d8(nada),.anodos(AN),.bcd(bcdout),.usados(uso));
     BCD toseven(.bcd(bcdout),.segmentos(seg));
     
 endmodule
