00d57d40f00d5f2edbe95cae01e92f4bec883add
jcontrerasf <jcontrerasf@users.noreply.github.com>
2019-07-24 17:23:26 -0400

    sesion 6 lista

--- ALU_polaca/ALU_polaca.srcs/sources_1/new/main.sv
+++ ALU_polaca/ALU_polaca.srcs/sources_1/new/main.sv
@@ -49,16 +49,16 @@ module main(
     .d2(dec[7:4]),
     .d3(dec[11:8]),
     .d4(dec[15:12]),
-    .d5(dec[19:16]),.d6(nada),.d7(nada),.d8(nada),.anodos(AN),.bcd(bcdout),.usados(uso));
+    .d5(dec[19:16]),.d6(op),.d7(b),.d8(a),.anodos(AN),.bcd(bcdout),.usados(uso));
     BCD toseven(.bcd(bcdout),.segmentos(seg));
 	FSM maquina(.RG({LED16_R,LED16_G}),.usados(uso),.clock(CLK100MHZ),.reset(~CPU_RESET),.invalido(inv),.siguiente(centro),.undo(abajo),.activar_reg(registros),.res_alu(resultado),.switches(SW),.mostrar(mostrar));
 	ALU alu(.A(a),.B(b),.botones(op),.salida(resultado),.invalido(inv));
 	debouncer btn_central(.clk(CLK100MHZ),.rst(~CPU_RESET),.PB(BTNC),.PB_pressed_pulse(centro));
 	debouncer btn_derecho(.clk(CLK100MHZ),.rst(~CPU_RESET),.PB(BTND),.PB_pressed_pulse(abajo));
 	debouncer btn_up(.clk(CLK100MHZ),.rst(~CPU_RESET),.PB(BTNU),.PB_pressed_status(up));
-	banco_de_registro bancoA(.guardar(centro),.clock(CLK100MHZ),.reset(~CPU_RESET),.entrada(SW),.salida(a),.enable(registros[0]));
+	banco_de_registro bancoA(.guardar(centro),.clock(CLK100MHZ),.reset(~CPU_RESET),.entrada(SW),.salida(a),.enable(registros[2]));
 	banco_de_registro bancoB(.guardar(centro),.clock(CLK100MHZ),.reset(~CPU_RESET),.entrada(SW),.salida(b),.enable(registros[1]));
-	banco_de_registro #(.bits(2)) bancoOP(.guardar(centro),.clock(CLK100MHZ),.reset(~CPU_RESET),.entrada(SW[1:0]),.salida(op),.enable(registros[2]));
+	banco_de_registro #(.bits(2)) bancoOP(.guardar(centro),.clock(CLK100MHZ),.reset(~CPU_RESET),.entrada(SW[1:0]),.salida(op),.enable(registros[0]));
 	dec_switcher switcher(.cambiar(up),.clock(CLK100MHZ),.hex(mostrar),.salida(dec),.trigger(1'b1));
     
 endmodule
